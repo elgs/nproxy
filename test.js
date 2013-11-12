86ba2f63637398d8241d56b19b97aa763c944015
Elgs Chen <elgs1980@hotmail.com>
2013-11-13 01:54:06 +0800

    test.js added.

---
+++ test.js
@@ -0,0 +1,31 @@
+var npr = require('./npr-kernel');
+
+var configProxy1 = {
+    "dstPort": 80,
+    "localPort": 3000,
+    "dstAddr": "www.microsoft.com"
+};
+
+var configProxy2 = {
+    "dstPort": 80,
+    "localPort": 2000,
+    "dstAddr": "www.google.com"
+};
+
+var configRouter = {
+    "localPort": 8000,
+    "routes": {
+        "hosta": {
+            "dstAddr": "127.0.0.1",
+            "dstPort": 10309
+        },
+        "default": {
+            "dstAddr": "127.0.0.1",
+            "dstPort": 10309
+        }
+    }
+};
+
+npr.run(configProxy1);
+npr.run(configProxy2);
+npr.run(configRouter);
